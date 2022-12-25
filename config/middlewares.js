8684445253ee0b44ad345179e5820e844b12d11f
Ivan Ilichev <swggnuko.dev@gmail.com>
2022-12-25 19:20:40 +0300

    added s3 bucket image storage

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,31 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'zy-aws-s3-images-bucket.s3.eu-west-2.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'zy-aws-s3-images-bucket.s3.eu-west-2.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
